9b53df584e6d442795148b4992f13516ffa9910f
caleffa <caleffa@gmail.com>
2024-04-20 14:39:55 -0300

    Preuba
    Archivo subido manualmente

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <tittle>Prueba de GIT</tittle>
+    </head>
+    <body>
+        <h2><strong>Bitweb</strong> GIT</h2>
+        <br>
+        <?php echo '<br><h3>Bienvenido!</h3>'; ?>
+        <?php echo '<br><h3>123</h3>'; ?>
+</body>
+</html>
